41dba783dd25821120f3a89a5f8d3b89c115eac6
ljungbladsolutions <christopher@ljungbladsolutions.se>
2023-04-01 10:43:49 +0200

    Added input field and function that takes the input url and generates a qrcode that is shown as an image, also added a download link so it is possible to download the qrcode image

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import QRCode from 'qrcode'
+import {useState} from "react";
+
 function App() {
+
+    const [url, setUrl] = useState('')
+    const [qrCode, setQrCode] = useState('')
+
+    const GenerateQRCode = () => {
+        QRCode.toDataURL(url, (err: any, url: any) => {
+            if (err) return console.error(err)
+
+            console.log(url)
+            setQrCode(url)
+        })
+    }
 
     return (
         <div className="app">
-            <h1>Hello world 2</h1>
+            <h1>QR Code Generator</h1>
+            <input
+                type="text"
+                placeholder="e.g. https://google.com"
+                value={url}
+                onChange={(evt) => setUrl(evt.target.value)}
+            />
+            <button onClick={GenerateQRCode}>Generate</button>
+            {qrCode && <>
+                <img src={qrCode}/>
+                <a href={qrCode} download="qrcode.png">Download</a>
+            </>
+            }
         </div>
     )
 }
